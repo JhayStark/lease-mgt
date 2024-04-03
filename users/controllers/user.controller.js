87461927820098912ad13ea77af88d8b81ad49b0
JhayStark <jaamen5@gmail.com>
2024-04-03 12:16:19 +0000

    chore:head seacrh by id

--- users/controllers/user.controller.js
+++ users/controllers/user.controller.js
@@ -68,8 +68,20 @@ const updateUser = async (req, res) => {
   }
 };
 
+const getUserByIDNumber = async (req, res) => {
+  try {
+    const idNumber = req.params.id;
+    const user = await User.findOne({ idNumber }).select(['-password']);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Internel server error' });
+  }
+};
+
 module.exports = {
   getUsers,
   getUser,
   updateUser,
+  getUserByIDNumber,
 };
--- users/routes/user.route.js
+++ users/routes/user.route.js
@@ -3,10 +3,12 @@ const {
   getUsers,
   getUser,
   updateUser,
+  getUserByIDNumber,
 } = require('../controllers/user.controller');
 
 userRouter.get('/all', getUsers);
 userRouter.get('/:id', getUser);
+userRouter.get('/head-search/:id', getUserByIDNumber);
 userRouter.patch('/:id', updateUser);
 
 module.exports = userRouter;
